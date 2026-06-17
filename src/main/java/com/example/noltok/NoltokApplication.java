cb08cfcdd5712d0a5a953f662a2beb2856aa3415
myoma <qq8380qq@gmail.com>
2026-06-17 22:28:22 +0900

    init: create noltok project

---
+++ src/main/java/com/example/noltok/NoltokApplication.java
@@ -0,0 +1,13 @@
+package com.example.noltok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoltokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoltokApplication.class, args);
+    }
+
+}
